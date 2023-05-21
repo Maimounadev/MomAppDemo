7198e97a4755afb3ceac0ee36dd03c68958d6de8
Maimounadev <maimounatraoredev@gmail.com>
2023-05-21 00:12:14 -0400

    styling changes

--- app/routes.js
+++ app/routes.js
@@ -1,213 +1,320 @@
+const { result } = require("lodash");
 const { ObjectId } = require("mongodb");
 // Set environment variables for your credentials: PUT THESE SECRETS IN A .ENV FILE BEFORE PUSHING
 const accountSid = "AC2378d3d252c198052c00a4d3bafd6d38";
 const authToken = "646c251363ef5ad393f95e4d5a0e8681";
 const client = require("twilio")(accountSid, authToken);
-const { Configuration, OpenAIApi } = require('openai')
+const { Configuration, OpenAIApi } = require("openai");
 
+module.exports = function (app, passport, db, multer) {
+  // Image Upload Code =========================================================================
+  var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images/uploads");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + "-" + Date.now() + ".png");
+    },
+  });
+  var upload = multer({ storage: storage });
 
-module.exports = function(app, passport, db, multer) {
-// Image Upload Code =========================================================================
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/images/uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + ".png")
-  }
-});
-var upload = multer({storage: storage});
+  // normal routes ===============================================================
 
-
-// normal routes ===============================================================
-
-    // show the home page (will also have our login links)
-    // starter api for my marketplace feature, users can buy and sell products. (more work to be done during the week, i also still have to work on the login page.)
-    app.get('/', function(req, res) {
-        res.render('signup.ejs');
-    });
-
-    app.get('/groupChat', isLoggedIn, function(req,res){
-      res.render('chat.ejs', {
-        user: req.user.local
-      })
-    })
-
-    app.get('/enterChat', isLoggedIn, function(req,res){
-      res.render('enterChat.ejs', {
-        user: req.user.local
-      })
-    })
-
-    app.get('/marketplace', isLoggedIn, function(req, res) {
-      db.collection('marketplace').find().toArray((err, items) => {
-        if (err) return console.log(err)
-        res.render('marketplace.ejs', {
-          items, 
-          myAddress: req.user.local.address
-        });
-      })
+  // show the home page (will also have our login links)
+  // starter api for my marketplace feature, users can buy and sell products. (more work to be done during the week, i also still have to work on the login page.)
+  app.get("/", function (req, res) {
+    res.render("signup.ejs");
   });
 
-  app.post('/sell', isLoggedIn,upload.single('file-to-upload'), (req, res) => {
-    const { title, price, address, description } = req.body 
-    db.collection('marketplace').save({title, price: Number(price), address, seller: req.user.local.email, description, imgPath: 'images/uploads/' + req.file.filename}, (err, result) => {
-      if (err) return console.log(err)
-      console.log('saved to database')
-      res.redirect('/marketplace')
-    })
-  })
+  app.get("/groupChat", isLoggedIn, function (req, res) {
+    res.render("chat.ejs", {
+      user: req.user.local,
+    });
+  });
 
-  
+  app.get("/enterChat", isLoggedIn, function (req, res) {
+    res.render("enterChat.ejs", {
+      user: req.user.local,
+    });
+  });
 
-  
-  
+  app.get("/marketplace", isLoggedIn, function (req, res) {
+    let boughtItems = [];
+    db.collection("marketplace")
+      .find()
+      .toArray((err, items) => {
+        db.collection("purchased")
+          .find()
+          .toArray((err, result) => {
+            if (err) return console.log(err);
+            for (let i = 0; i < result.length; i++) {
+              boughtItems.push(result[i].item[0]._id);
+            }
+            res.render("marketplace.ejs", {
+              items,
+              result,
+              boughtItems: boughtItems,
+              myAddress: req.user.local.address,
+            });
+          });
+      });
+  });
 
-   app.get('/purchased', isLoggedIn, function(req, res) {
-      db.collection('purchased').find({user: req.user.local.email}).toArray((err, data) => {
-        console.log(data)
-        if (err) return console.log(err)
-        res.render('purchased.ejs', {
+  app.get("/marketplace/:price", isLoggedIn, function (req, res) {
+    let boughtItems = [];
+    db.collection("marketplace")
+      .find({ price: parseInt(req.params.price )})
+      .toArray((err, items) => {
+        db.collection("purchased")
+          .find()
+          .toArray((err, result) => {
+            if (err) return console.log(err);
+            for (let i = 0; i < result.length; i++) {
+              boughtItems.push(result[i].item[0]._id);
+            }
+            res.render("marketplace.ejs", {
+              items,
+              result,
+              boughtItems: boughtItems,
+              myAddress: req.user.local.address,
+            });
+          
+          });
+      });
+  });
+
+  app.post("/sell", isLoggedIn, upload.single("file-to-upload"), (req, res) => {
+    const { title, price, address, description } = req.body;
+    db.collection("marketplace").save(
+      {
+        isAvailabale: true,
+        buyer: "noBody",
+        title,
+        price: Number(price),
+        address,
+        seller: req.user.local.email,
+        description,
+        imgPath: "images/uploads/" + req.file.filename,
+      },
+      (err, result) => {
+        if (err) return console.log(err);
+        console.log("saved to database");
+        res.redirect("/marketplace");
+      }
+    );
+  });
+
+  app.get("/purchased", isLoggedIn, function (req, res) {
+    db.collection("purchased")
+      .find({ user: req.user.local.email })
+      .toArray((err, data) => {
+        console.log(data);
+        if (err) return console.log(err);
+        res.render("purchased.ejs", {
           data,
           myAddress: req.user.local.address,
         });
-      })
+      });
   });
 
-  app.post('/purchase/:id', isLoggedIn, (req, res) => {
-  const item =  db.collection('marketplace').find({_id: ObjectId(req.params.id)}).toArray((err, item) => {
-    db.collection('purchased').save({item, purchasedOn: new Date(), user: req.user.local.email}, (err, result) => {
-      if (err) return res.send(err)
-      res.redirect('/purchased')
-})
-    })
-  });
-
-  app.delete('/marketplace', isLoggedIn, (req, res) => {
-    const _id = ObjectId(req.body._id)
-    db.collection('marketplace').findOneAndDelete({ _id}, (err, result) => {
-      if (err) return res.send(500, err)
-      res.send('Message deleted!')
-    })
-  })
-
-    // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, function(req, res) {
-        db.collection('messages').find().toArray((err, result) => {
-          if (err) return console.log(err)
-          const isNewMom = req.user.local.momType == 'first-time-mom'
-          // if (isNewMom) {
-          //   res.render('newMomProfile.ejs')
-          // }
-          // else{
-            res.render('profile.ejs', {
-              user : req.user.local,
-              messages: result
-            })
-          // }
-  
-        })
-    });
-
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-        req.logout(() => {
-          console.log('User has logged out!')
-        });
-        res.redirect('/');
-    });
-
-// message board routes ===============================================================
-
-    
-
-    
-    
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
-
-    // locally --------------------------------
-        // LOGIN ===============================
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
-
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
-
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/welcome-msg', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        })
+  app.post("/purchase/:id", isLoggedIn, (req, res) => {
+    const item = db
+      .collection("marketplace")
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, item) => {
+        db.collection("purchased").save(
+          { item, purchasedOn: new Date(), user: req.user.local.email },
+          (err, result) => {
+            if (err) return res.send(err);
+            res.redirect("/purchased");
+          }
         );
-        // open ai 
+      });
+  });
 
-        app.post('/getAi', async (req, res) => {
-          console.log(req.body.ingredient)
-      
-          const configuration = new Configuration({
-            apiKey: process.env.OPENAI_API_KEY,
-          });
-          const openai = new OpenAIApi(configuration);
-          const ingredients = req.body.ingredient
-          let prompt = 'Give me a recipe using only these ingredients:'
-          ingredients.forEach(food => prompt += food + ',')
-          console.log(prompt)
-          const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: prompt }],
-          });
-          const recipe = completion.data.choices[0].message.content
-          console.log(completion.data.choices[0].message);
-          res.render('recipe.ejs', {recipe})
-        })
-      
-      
-
-        app.get('/welcome-msg', function(req,res){
-           client.messages
-           .create({ body: "Welcome to my app", from: "+18886045109", to: "+16103875392" })
-           .then(message => {
-            console.log('message sent!', message.sid);
-            res.redirect('/profile')
-          })
-          .catch(error =>
-            console.error(error)
-          )
-        })
-
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
-
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
+  app.delete("/marketplace", isLoggedIn, (req, res) => {
+    const _id = ObjectId(req.body._id);
+    db.collection("marketplace").findOneAndDelete({ _id }, (err, result) => {
+      if (err) return res.send(500, err);
+      res.send("Message deleted!");
     });
+  });
 
+  // PROFILE SECTION =========================
+  app.get("/profile", isLoggedIn, function (req, res) {
+    db.collection("messages")
+      .find()
+      .toArray((err, result) => {
+        if (err) return console.log(err);
+        const isNewMom = req.user.local.momType == "first-time-mom";
+        // if (isNewMom) {
+        //   res.render('newMomProfile.ejs')
+        // }
+        // else{
+        res.render("profile.ejs", {
+          user: req.user.local,
+          messages: result,
+        });
+        // }
+      });
+  });
+
+  // LOGOUT ==============================
+  app.get("/logout", function (req, res) {
+    req.logout(() => {
+      console.log("User has logged out!");
+    });
+    res.redirect("/");
+  });
+
+  // message board routes ===============================================================
+
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // process the login form
+  app.post(
+    "/login",
+    passport.authenticate("local-login", {
+      successRedirect: "/profile", // redirect to the secure profile section
+      failureRedirect: "/login", // redirect back to the signup page if there is an error
+      failureFlash: true, // allow flash messages
+    })
+  );
+
+  // SIGNUP =================================
+  // show the signup form
+  app.get("/signup", function (req, res) {
+    res.render("signup.ejs", { message: req.flash("signupMessage") });
+  });
+
+  // process the signup form
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/welcome-msg", // redirect to the secure profile section
+      failureRedirect: "/signup", // redirect back to the signup page if there is an error
+      failureFlash: true, // allow flash messages
+    })
+  );
+
+  // Calendar routes ===============================================================
+
+  // Display the calendar page
+  const { ObjectId } = require("mongodb");
+
+  app.get("/calendar", isLoggedIn, function (req, res) {
+    // Retrieve events from the database
+    db.collection("events")
+      .find()
+      .toArray((err, events) => {
+        if (err) {
+          console.log(err);
+          return res
+            .status(500)
+            .send("Error retrieving events from the database.");
+        }
+        res.render("calendar.ejs", { events });
+      });
+  });
+
+  app.post("/calendar/event", isLoggedIn, function (req, res) {
+    const { title, date, description } = req.body;
+
+    // Save the new event to the database
+    db.collection("events").insertOne(
+      { title, date, description },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send("Error saving event to the database.");
+        }
+        console.log("Event saved to the database");
+        res.redirect("/calendar");
+      }
+    );
+  });
+
+  app.delete("/calendar/event/:id", isLoggedIn, function (req, res) {
+    const eventId = req.params.id;
+
+    // Delete the event from the database
+    db.collection("events").deleteOne(
+      { _id: ObjectId(eventId) },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return res
+            .status(500)
+            .send("Error deleting event from the database.");
+        }
+        res.send("Event deleted!");
+      }
+    );
+  });
+
+  // ...
+
+  // open ai
+
+  app.post("/getAi", async (req, res) => {
+    console.log(req.body.ingredient);
+
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const ingredients = req.body.ingredient;
+    let prompt = "Give me a recipe using only these ingredients:";
+    ingredients.forEach((food) => (prompt += food + ","));
+    console.log(prompt);
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+    const recipe = completion.data.choices[0].message.content;
+    console.log(completion.data.choices[0].message);
+    res.render("recipe.ejs", { recipe });
+  });
+
+  app.get("/welcome-msg", function (req, res) {
+    client.messages
+      .create({
+        body: "Welcome to my app",
+        from: "+18886045109",
+        to: "+16103875392",
+      })
+      .then((message) => {
+        console.log("message sent!", message.sid);
+        res.redirect("/profile");
+      })
+      .catch((error) => console.error(error));
+  });
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get("/unlink/local", isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect("/profile");
+    });
+  });
 };
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
+  if (req.isAuthenticated()) return next();
 
-    res.redirect('/');
+  res.redirect("/");
 }
--- public/marketplace.js
+++ public/marketplace.js
@@ -1,36 +1,35 @@
 //apprearance
-document.querySelectorAll("input.variation").forEach(function(element) {
-  element.addEventListener("click", function() {
-  if (parseInt(this.value) > 3) {
-  document.body.style.background = "#111";
-  document.querySelector("footer").className = "dark";
-  } else {
-  document.body.style.background = "#f9f9f9";
-  }
+document.querySelectorAll("input.variation").forEach(function (element) {
+  element.addEventListener("click", function () {
+    if (parseInt(this.value) > 3) {
+      document.body.style.background = "#111";
+      document.querySelector("footer").className = "dark";
+    } else {
+      document.body.style.background = "#f9f9f9";
+    }
   });
+});
+
+// toggle list vs card view
+document.querySelectorAll(".option__button").forEach(function (element) {
+  element.addEventListener("click", function () {
+    document.querySelectorAll(".option__button").forEach(function (button) {
+      button.classList.remove("selected");
+    });
+    this.classList.add("selected");
+    var resultsSection = document.querySelector(".results-section");
+    if (this.classList.contains("option--grid")) {
+      resultsSection.className = "results-section results--grid";
+    } else if (this.classList.contains("option--list")) {
+      resultsSection.className = "results-section results--list";
+    }
   });
-  
-  // toggle list vs card view
-  document.querySelectorAll(".option__button").forEach(function(element) {
-  element.addEventListener("click", function() {
-  document.querySelectorAll(".option__button").forEach(function(button) {
-  button.classList.remove("selected");
-  });
-  this.classList.add("selected");
-  var resultsSection = document.querySelector(".results-section");
-  if (this.classList.contains("option--grid")) {
-  resultsSection.className = "results-section results--grid";
-  } else if (this.classList.contains("option--list")) {
-  resultsSection.className = "results-section results--list";
-  }
-  });
-  });
+});
 
 document.querySelector(".sell").addEventListener("click", postItem);
 
-
 const form = document.querySelector(".form");
-console.log(form)
+console.log(form);
 form.style.display = "none";
 
 function postItem() {
@@ -38,17 +37,41 @@ function postItem() {
 }
 
 function deleteItem(_id) {
-    fetch('marketplace', {
-      method: 'delete',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        _id
-      })
-    }).then(function (response) {
-      window.location.reload()
-    })
-  ;
+  fetch("marketplace", {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      _id,
+    }),
+  }).then(function (response) {
+    window.location.reload();
+  });
 }
+// price filters
+const priceFilterInputs = Array.from(document.getElementsByClassName('priceFilter'))
+priceFilterInputs.forEach(function (element) {
+  element.addEventListener("click",filterByPrice);
+});
 
+function filterByPrice(e) {
+  // e.preventDefault()
+  let price = e.target.value;
+  let filterPage = window.location.href + "/" + price;
+
+  fetch("marketplace/" + price, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      console.log(response)
+      if (response.ok) return response;
+    })
+    .then(function (data) {
+      console.log(data)
+      window.location.assign(data.url);
+    });
+}
